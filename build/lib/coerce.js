dfd8225385e0ea30ade9773253b21956c40f5da8
Johannes Krobath <krobi@power-dreams.com>
2026-05-06 00:03:07 +0200

    chore: release v1.22.0
    - **Interne Aufräumung**: `safeStringEqual` (timing-safe Credentials-Vergleich) ist von `webserver.ts` nach `coerce.ts` umgezogen — generischer Crypto-Helper. Plus 5 neue Unit-Tests.

--- build/lib/coerce.js
+++ build/lib/coerce.js
@@ -1,7 +1,9 @@
 "use strict";
+var __create = Object.create;
 var __defProp = Object.defineProperty;
 var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
 var __getOwnPropNames = Object.getOwnPropertyNames;
+var __getProtoOf = Object.getPrototypeOf;
 var __hasOwnProp = Object.prototype.hasOwnProperty;
 var __export = (target, all) => {
   for (var name in all)
@@ -15,6 +17,14 @@ var __copyProps = (to, from, except, desc) => {
   }
   return to;
 };
+var __toESM = (mod, isNodeMode, target) => (target = mod != null ? __create(__getProtoOf(mod)) : {}, __copyProps(
+  // If the importer is in node compatibility mode or this is not an ESM
+  // file that has been converted to a CommonJS file using a Babel-
+  // compatible transform (i.e. "__esModule" has not been set), then set
+  // "default" to the CommonJS "module.exports" for node compatibility.
+  isNodeMode || !mod || !mod.__esModule ? __defProp(target, "default", { value: mod, enumerable: true }) : target,
+  mod
+));
 var __toCommonJS = (mod) => __copyProps(__defProp({}, "__esModule", { value: true }), mod);
 var coerce_exports = {};
 __export(coerce_exports, {
@@ -29,9 +39,16 @@ __export(coerce_exports, {
   isPlainObject: () => isPlainObject,
   parseAdapterStateId: () => parseAdapterStateId,
   parseManualUrlWrite: () => parseManualUrlWrite,
-  safeGetState: () => safeGetState
+  safeGetState: () => safeGetState,
+  safeStringEqual: () => safeStringEqual
 });
 module.exports = __toCommonJS(coerce_exports);
+var import_node_crypto = __toESM(require("node:crypto"));
+function safeStringEqual(a, b) {
+  const ah = import_node_crypto.default.createHash("sha256").update(a, "utf8").digest();
+  const bh = import_node_crypto.default.createHash("sha256").update(b, "utf8").digest();
+  return import_node_crypto.default.timingSafeEqual(ah, bh);
+}
 function isNoChoice(value) {
   return value === 0 || value === "0" || value === "";
 }
@@ -142,6 +159,7 @@ function buildDropdownStates(sentinels, urlStates) {
   isPlainObject,
   parseAdapterStateId,
   parseManualUrlWrite,
-  safeGetState
+  safeGetState,
+  safeStringEqual
 });
 //# sourceMappingURL=coerce.js.map
